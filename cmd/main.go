eb9622db98be03a62940cd77562f8060124dea51
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-28 17:29:08 -0400

    start using echo framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
+import (
+	"github.com/labstack/echo/v4"
+)
 
+const (
+	portNumber = ":8080"
+)
+
+var app = echo.New()
+
+func main() {
+	run()
+}
+
+func run() {
+	routes()
+	app.Logger.Fatal(app.Start(portNumber))
 }
