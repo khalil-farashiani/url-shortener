cfa59ff80e6367f874ce08ba98df2f55abf2f718
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-29 11:26:30 -0400

    add error param in run function

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,10 +13,14 @@ const (
 var app = echo.New()
 
 func main() {
-	run()
+	err := run()
+	if err != nil {
+		panic(fmt.Sprintf("Error while trying to start application: %s", err.Error()))
+	}
 }
 
-func run() {
+func run() error {
 	routes()
 	app.Logger.Fatal(app.Start(portNumber))
+	return nil
 }
