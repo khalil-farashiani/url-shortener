ca3150462b6538ab6367b0e80f3223d38b1c59de
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-29 20:56:19 -0400

    complete gorm to connect mysql database via driver package

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/khalil-farashiani/url-shortener/internal/models/url"
-	"github.com/khalil-farashiani/url-shortener/internal/models/user"
-
+	"github.com/khalil-farashiani/url-shortener/internal/drivers"
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 const (
@@ -25,17 +21,13 @@ func main() {
 }
 
 func run() error {
-	fmt.Println("im here")
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	//connect to database
+	err := drivers.ConnectSQL()
 	if err != nil {
 		return err
 	}
-
-	//migrate th User and Url entity schema
-	db.AutoMigrate(&user.User{})
-	db.AutoMigrate(&url.Url{})
-
 	routes()
+
 	app.Logger.Fatal(app.Start(portNumber))
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/labstack/echo/v4 v4.7.2
+	gorm.io/driver/mysql v1.3.2 // indirect
 	gorm.io/driver/sqlite v1.3.1
 	gorm.io/gorm v1.23.3
 )
