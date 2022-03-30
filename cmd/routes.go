9bda4ab3eec526ced2eb8e8d70a8235ed2fe081c
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-30 10:30:27 -0400

    add CRUD user and url routes

--- cmd/routes.go
+++ cmd/routes.go
@@ -4,4 +4,18 @@ import "github.com/khalil-farashiani/url-shortener/internal/handlers"
 
 func routes() {
 	app.GET("/ping", handlers.Ping)
+	//url routes
+	app.POST("/urls", handlers.CreateUrl)
+	app.GET("/urls/:url_id", handlers.GetUrl)
+	app.PUT("/urls", handlers.UpdateUrl)
+	app.PATCH("/urls", handlers.UpdateUrl)
+	app.DELETE("/urls", handlers.DeleteUrl)
+	app.GET("/urls/?created_at", handlers.SearchUrl)
+	//user routes
+	app.POST("/users", handlers.CreateUser)
+	app.GET("/users/:url_id", handlers.GetUser)
+	app.PUT("/users", handlers.UpdateUser)
+	app.PATCH("/users", handlers.UpdateUser)
+	app.DELETE("/users", handlers.DeleteUser)
+	app.GET("/users/?created_at", handlers.SearchUser)
 }
