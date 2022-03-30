1951ae58413a6576139267e3dd815a42ed9dd98d
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-30 11:54:59 -0400

    continue to implement create user handler

--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/khalil-farashiani/url-shortener/internal/drivers"
@@ -9,10 +10,25 @@ import (
 )
 
 func CreateUser(c echo.Context) error {
-	name := c.FormValue("name")
+	username := c.FormValue("username")
 	email := c.FormValue("email")
-	drivers.DB.Create(&user.User{})
-	return c.JSON(http.StatusNotImplemented, "implement me!")
+	password := c.FormValue("password")
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("error while getting avatar %s", err.Error()))
+	}
+	user := &user.User{
+		Username: username,
+		Email:    &email,
+		Password: password,
+		Avatar:   &avatar.Filename,
+	}
+	if err := user.Validate(); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	drivers.DB.Create(user)
+	return c.JSON(http.StatusCreated, "user created")
 }
 
 func GetUser(c echo.Context) error {
