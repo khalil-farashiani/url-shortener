aea8d0f60b46cd14cd479f2efc9d44453a08ede8
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-30 10:31:05 -0400

    change in my sql driver and create a DB var to use in other package

--- internal/drivers/mysql_driver.go
+++ internal/drivers/mysql_driver.go
@@ -1,7 +1,6 @@
 package drivers
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/khalil-farashiani/url-shortener/internal/models/url"
@@ -11,22 +10,19 @@ import (
 	"gorm.io/gorm"
 )
 
-// DB holds the database connection pool
-type DB struct {
-	SQL *sql.DB
-}
-
 var (
 	username = utils.GetEnv("my_sql_username", "root")
 	password = utils.GetEnv("my_sql_password", "root")
 	host     = utils.GetEnv("my_sql_host", "127.0.0.1:3306")
 	schema   = utils.GetEnv("my_sql_schema", "sholink")
+
+	DB = &gorm.DB{}
 )
 
 func ConnectSQL() error {
 	var err error
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, schema)
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
 	}
