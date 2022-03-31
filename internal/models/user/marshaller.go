351130455a44f930a5c13150406791b049d715b8
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-31 14:02:03 -0400

    add marshaller for user to response

---
+++ internal/models/user/marshaller.go
@@ -0,0 +1,17 @@
+package user
+
+import "encoding/json"
+
+type MarshallUser struct {
+	Id       int64   `json:"id"`
+	Username string  `json:"user_name"`
+	Email    *string `json:"email"`
+	Avatar   *string `json:"avatar"`
+}
+
+func (user User) Marshall() MarshallUser {
+	userJson, _ := json.Marshal(user)
+	result := MarshallUser{}
+	json.Unmarshal(userJson, &result)
+	return result
+}
