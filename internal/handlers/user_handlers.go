65a9bc10aeb45b5564221ef97cfa9516093d0879
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-03-31 14:36:50 -0400

    complete create user flow

--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -1,34 +1,77 @@
 package handlers
 
 import (
-	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/khalil-farashiani/url-shortener/internal/drivers"
 	"github.com/khalil-farashiani/url-shortener/internal/models/user"
+	"github.com/khalil-farashiani/url-shortener/internal/utils"
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	userAssets = "assets/user/"
+)
+
 func CreateUser(c echo.Context) error {
+	// create a user struct
+	var user = &user.User{}
+
 	username := c.FormValue("username")
 	email := c.FormValue("email")
 	password := c.FormValue("password")
+
 	// Get avatar
 	avatar, err := c.FormFile("avatar")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("error while getting avatar %s", err.Error()))
+	if avatar != nil && err != nil {
+		return echo.NewHTTPError(
+			http.StatusInternalServerError,
+			utils.NewInternalServerError("unable to save avatar"),
+		)
+	} else if avatar != nil {
+		src, err := avatar.Open()
+		if err != nil {
+			return echo.NewHTTPError(
+				http.StatusInternalServerError,
+				utils.NewInternalServerError("unable to save avatar"),
+			)
+		}
+		defer src.Close()
+
+		dst, err := os.Create(userAssets + avatar.Filename)
+		if err != nil {
+			return echo.NewHTTPError(
+				http.StatusInternalServerError,
+				utils.NewInternalServerError("unable to save avatar"),
+			)
+		}
+
+		if _, err = io.Copy(dst, src); err != nil {
+			return echo.NewHTTPError(
+				http.StatusInternalServerError,
+				utils.NewInternalServerError("unable to save avatar"),
+			)
+		}
+
+		defer dst.Close()
+		userAvatar := userAssets + avatar.Filename
+		user.Avatar = &userAvatar
 	}
-	user := &user.User{
-		Username: username,
-		Email:    &email,
-		Password: password,
-		Avatar:   &avatar.Filename,
-	}
+
+	user.Username = username
+	user.Password = utils.GetMD5(password)
+	user.Email = &email
+
 	if err := user.Validate(); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, utils.NewBadRequestError(err.Error()))
 	}
-	drivers.DB.Create(user)
-	return c.JSON(http.StatusCreated, "user created")
+	err = drivers.DB.Create(user).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewBadRequestError("this user already exists"))
+	}
+	return c.JSON(http.StatusCreated, user.Marshall())
 }
 
 func GetUser(c echo.Context) error {
