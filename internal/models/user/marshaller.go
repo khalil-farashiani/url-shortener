4e4653165ec61f0d418a8110636a553c0d0ac458
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-04-01 08:03:16 -0400

    add urls of users to marshaller

--- internal/models/user/marshaller.go
+++ internal/models/user/marshaller.go
@@ -1,12 +1,17 @@
 package user
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/khalil-farashiani/url-shortener/internal/models/url"
+)
 
 type MarshallUser struct {
-	Id       int64   `json:"id"`
-	Username string  `json:"user_name"`
-	Email    *string `json:"email"`
-	Avatar   *string `json:"avatar"`
+	Id       int64     `json:"id"`
+	Username string    `json:"user_name"`
+	Email    *string   `json:"email"`
+	Avatar   *string   `json:"avatar"`
+	Url      []url.Url `json:"urls"`
 }
 
 func (user User) Marshall() MarshallUser {
