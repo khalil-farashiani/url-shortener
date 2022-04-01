e0df1877c5c6b315ef4ad91999544a51210cdb13
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-04-01 07:59:35 -0400

    add user get and destroy handlers

--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/khalil-farashiani/url-shortener/internal/drivers"
 	"github.com/khalil-farashiani/url-shortener/internal/models/user"
@@ -12,8 +14,18 @@ import (
 )
 
 const (
-	userAssets = "assets/user/"
+	userAssets = `assets/user/`
 )
+
+func getUserId(userIdParam string) (int64, *utils.RestErr) {
+	userId, userErr := strconv.ParseInt(userIdParam, 10, 64)
+	if userErr != nil {
+		err := utils.NewBadRequestError("user id should be a number")
+		return 0, err
+	}
+
+	return userId, nil
+}
 
 func CreateUser(c echo.Context) error {
 	// create a user struct
@@ -75,11 +87,35 @@ func CreateUser(c echo.Context) error {
 }
 
 func GetUser(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, "implement me!")
+	idParam := c.Param("user_id")
+	userId, getErr := getUserId(idParam)
+	if getErr != nil {
+		return c.JSON(getErr.Status, getErr)
+	}
+	user := &user.User{}
+	err := drivers.DB.First(&user, userId).Error
+	if err != nil {
+		return c.JSON(http.StatusNotFound, utils.NewBadRequestError("user not found"))
+	}
+
+	return c.JSON(http.StatusOK, user.Marshall())
 }
 
 func DeleteUser(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, "implement me!")
+	idParam := c.Param("user_id")
+	userId, getErr := getUserId(idParam)
+	if getErr != nil {
+		return c.JSON(getErr.Status, getErr)
+	}
+
+	user := &user.User{}
+	err := drivers.DB.Delete(&user, userId).Error
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.JSON(http.StatusNotFound, utils.NewBadRequestError("user not found"))
+	}
+
+	return c.JSON(http.StatusOK, "user deleted")
 }
 
 func SearchUser(c echo.Context) error {
