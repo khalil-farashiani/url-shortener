12ad2969e227ba671fce1ad7536e0cf83b2f5095
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-04-06 14:51:43 -0400

    delete UrlId field of url model

--- internal/models/url/url.go
+++ internal/models/url/url.go
@@ -7,7 +7,6 @@ import (
 
 type Url struct {
 	gorm.Model
-	UrlId    uint64    `json:"url_id" gorm:"primaryKey;auto_increment;constraint:OnDelete:SET NULL;"`
 	Source   string    `json:"source"`
 	ShortUrl string    `json:"short_url" gorm:"unique"`
 	UserID   uint64    `json:"user_id"`
